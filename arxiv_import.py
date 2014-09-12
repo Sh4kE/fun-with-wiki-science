8dc5bef2d65d1d80d890d166e1f356d88b67f750
Michael Wittig <michael.wittig@fu-berlin.de>
2014-09-12 15:29:20 +0200

    fixing import

--- arxiv_import.py
+++ arxiv_import.py
@@ -4,7 +4,8 @@ import config as c
 from pymongo import MongoClient
 
 client = MongoClient()
-db = client.articles
+db = client.articles.entries
+
 def generate_filename(entry, directory = ''):
   authors = [a.split()[-1] for a in entry['authors']]
   authors = authors[0]+'_et.al' if len(authors) > 1 else authors[0]
@@ -15,17 +16,18 @@ def dl_pdf_from_arxiv(url):
   html_doc = urllib.urlopen(url).read()
   s = BeautifulSoup(html_doc)
   entries = [{
-    'pdf' : e.findAll('link',attrs={'type': 'application/pdf'})[0]['href'], 
+    'pdf' : e.findAll('link',attrs={'type': 'application/pdf'})[0]['href'],
     'url' : e.findAll('link',attrs={'type': 'text/html'})[0]['href'],
-    'authors': [str(a.next.next.next) for a in e.findAll('author')], 
+    'authors': [str(a.next.next.next) for a in e.findAll('author')],
     'title': str(e.title.next),
     'id': str.split(str(e.id.next),'/')[-1]
             } for e in s.findAll('entry')]
-  entries = filter(lambda e: if db.find_one({'id': e['id']}) else False,entries)
+  entries = filter(lambda e: db.find_one({'id': e['id']}) == None, entries)
+  print "entries: ", entries
   map(lambda e: urllib.urlretrieve(e['pdf'], generate_filename(e, directory = c.ARTICLE_DIR)), entries)
-  
+
 
 def stripAllTags(html):
         if html is None:
                 return None
-        return ''.join( BeautifulSoup( html ).findAll() ) 
+        return ''.join( BeautifulSoup( html ).findAll() )
