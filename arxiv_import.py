41a274ac8f2c02bcfba5d702c0047e3e7fecc6a3
Marco Ziener <marco.ziener.mz@gmail.com>
2014-09-12 16:05:55 +0200

    meh

--- arxiv_import.py
+++ arxiv_import.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 client = MongoClient()
 db = client.articles.entries
 
-def generate_filename(entry, directory = ''):
+def generate_filename(entry, directory = c.ARTICLE_DIR):
   authors = [a.split()[-1] for a in entry['authors']]
   authors = authors[0]+'_et.al' if len(authors) > 1 else authors[0]
   title = entry['title'].replace(' ', '_')
@@ -22,9 +22,10 @@ def dl_pdf_from_arxiv(url):
     'title': str(e.title.next),
     'id': str.split(str(e.id.next),'/')[-1],
             } for e in s.findAll('entry')]
-  entries = filter(lambda e: db.find_one({'id': e['id']}) == None, 
-                   [entry['path'] = generate_filename(entry, directory = c.ARTICLE_DIR) for entry in entries)
-  map(lambda e: urllib.urlretrieve(e['pdf'], generate_filename(e, directory = c.ARTICLE_DIR)), entries)
+  entries = filter(lambda e: db.find_one({'id': e['id']}) == None, entries)
+  for entry in entries:
+    entry['path'] = generate_filename(entry)
+  map(lambda e: urllib.urlretrieve(e['pdf'], e['path']), entries)
   if entries:
     db.insert(entries)
 
