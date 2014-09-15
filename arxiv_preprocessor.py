c12354f32ff39a6fb3ccbd74097d9dacec638c20
Michael Wittig <michael.wittig@fu-berlin.de>
2014-09-15 11:46:07 +0200

    add function for update mongodb with pdf texts

--- arxiv_preprocessor.py
+++ arxiv_preprocessor.py
@@ -1,7 +1,13 @@
-from bs4 import BeautifulSoup
 import sh
+from pymongo import MongoClient
 
 pdf2txt = sh.Command('pdf2txt.py')
 
+def update_db_with_pdf_texts():
+  client = MongoClient()
+  db = client.articles.entries
+  [db.update({'_id' : entry['_id'], {'text': preprocess(entry)}) for entry in db.find()]
+
 def preprocess(entry):
-    result = pdf2txt(entry['
+    result = pdf2txt(entry['path'])
+    return result
