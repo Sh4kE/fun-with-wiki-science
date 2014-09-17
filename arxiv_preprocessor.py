59c6519259012373d397b1b64ac956aab9fccffa
Michael Wittig <michael.wittig@fu-berlin.de>
2014-09-17 14:31:13 +0200

    arxiv_import

--- arxiv_preprocessor.py
+++ arxiv_preprocessor.py
@@ -1,13 +1,16 @@
 import sh
 from pymongo import MongoClient
 
-pdf2txt = sh.Command('pdf2txt.py')
+pdf2txt = sh.Command('pdf2txt.py').bake('-c ascii')
 
 def update_db_with_pdf_texts():
   client = MongoClient()
   db = client.articles.entries
-  [db.update({'_id' : entry['_id'], {'text': preprocess(entry)}) for entry in db.find()]
+
+  for entry in db.find():
+    if not entry.has_key('text'):
+      db.save(preprocess(entry))
 
 def preprocess(entry):
-    result = pdf2txt(entry['path'])
-    return result
+    entry['text'] = pdf2txt(entry['path']).encode('ascii')
+    return entry
