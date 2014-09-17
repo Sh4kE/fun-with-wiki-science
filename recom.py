8b9670b9e45222499340e70dc7d0a3ff2d22726f
Marco Ziener <marco.ziener.mz@gmail.com>
2014-09-17 15:19:03 +0200

    started recommandation

---
+++ recom.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+from gensim import corpora, models, similarities
+
+db = MongoClient().articles.entries
+
+lda = gensim.models.LdaModel.load('data/lda_model')
+dic = gensim.corpora.Dictionary.load_from_text('data/wiki_en_wordids.txt')
+
+def entry2bow(entry):
+    return dic.doc2bow(entry['text'].lower().split())
+    
+def bow2topics(bow):
+    return lda[bow]
+
+def entry2topics(entry):
+    return bow2topics(entry2bow(entry))
+    
